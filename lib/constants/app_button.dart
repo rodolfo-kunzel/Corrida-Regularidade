78b635d86de3c885266f39625685c27790f59833
@rodolfo-kunzel <rodi.bkunzel@gmail.com>
2022-06-23 12:02:47 -0300

    register-car-layout
    Register car layout done

---
+++ lib/constants/app_button.dart
@@ -0,0 +1,36 @@
+import 'package:corrida_de_regulariodade_flutter/constants/app_constant_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppButton extends StatelessWidget {
+  final void Function() onTap;
+  const AppButton({Key? key, required this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        InkWell(
+          onTap: onTap,
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 250, maxHeight: 80),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color: AppConstantColors.appBlack),
+            child: Center(
+              child: Text(
+                'Confirmar',
+                style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    color: AppConstantColors.appOrange),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
